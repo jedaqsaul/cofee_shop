432e1a5bcb5dca549614c4cda0828d3a78972286
jedaqsaul <jedaqsaul@gmail.com>
2025-05-21 00:47:24 +0300

    defines the property setter for the coffee class

--- coffee.py
+++ coffee.py
@@ -9,4 +9,7 @@ class Coffee:
     
     @name.setter
     def name(self, value):
-        pass
+        if isinstance(value, str) and len(value) >=3:
+            self._name=value
+        else:# raise an error here
+            raise ValueError('Coffee is supposed to be a string with no less than 3 chars')
