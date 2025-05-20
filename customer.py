d31f1d927b9707ed8adae739a5fdd54e2ebef029
jedaqsaul <jedaqsaul@gmail.com>
2025-05-21 00:43:25 +0300

    Defines the setter property for the customer class

--- customer.py
+++ customer.py
@@ -5,9 +5,13 @@ class Customer:
 
     @property
     def name(self):
-        return self_name
+        return self._name
     
-    @name.setter
-    def name(self,value):
-        pass
+    @name.setter #this makes the method a setter for -name property
+    def name(self,value): #check if value is a string and its length is btw 1 and 17
+        # if all([isinstance(value, str), 1 <= len(value) <= 15]):
+        if isinstance(value, str) and 1<= len(value)<= 15:
+            self._name=value #set the private attribute _name if it is valid
+        else: #raise and error if value is not a valid string or the stipulated lenght
+            raise ValueError('The customers name should be a string between 1 and 15 chars')
    
