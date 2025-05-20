2956c0414a160605cbeeb36b3c325d82c01819f8
jedaqsaul <jedaqsaul@gmail.com>
2025-05-21 00:49:58 +0300

    import Customer and Coffee from their respective modules into order.py

---
+++ order.py
@@ -0,0 +1,8 @@
+# First thing- import Customer and Coffee from their files
+
+from customer import Customer
+from coffee import Coffee
+
+class Order:
+    # Represents an order placed by a customer for a coffee
+    pass
